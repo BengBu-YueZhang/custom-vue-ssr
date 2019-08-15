1292dbe30cd0231d09b5e30b9d68354976ecfec9
BengBu-YueZhang <zhangyue_coder@163.com>
2019-08-15 17:36:01 +0800

    更新代码

---
+++ server.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const Koa = require('koa')
+const Router = require('koa-router')
+const router = new Router()
+const LRU = require('lru-cache')
+const { createBundleRenderer } = require('vue-server-renderer')
+
+const app = new Koa()
+// 静态html模版
+const template = fs.readFileSync('./src/index.template.html', 'utf-8')
+
+const createRenderer = (serverBundle) => {
+  return createBundleRenderer(serverBundle, {
+    // 静态组件缓存
+    cache: LRU({
+      max: 1000,
+      maxAge: 1000 * 60 * 20
+    }),
+    runInNewContext: false
+  })
+}
+
+const render = (ctx) => {
+}
+
+router.get('*', render)
+
+app.use(router.routes(), router.allowedMethods())
+
+app.listen(8080)
